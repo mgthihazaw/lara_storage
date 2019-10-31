032b47e001c25d06ab746e273a9f9ccefd99be47
mgthihazaw <mgthihazaw777@gmail.com>
2019-10-31 13:20:20 +0630

    add signed url

--- routes/web.php
+++ routes/web.php
@@ -24,9 +24,27 @@ Laravel Redis
 */
 
 use Illuminate\Support\Facades\Redis;
+use Illuminate\Support\Facades\URL;
 
 Route::get('/redis', function () {
-    $redis = app()->make('redis');
-    // $redis->set("key1", "TEST");
-    return $redis->get("key1");
+    // $redis = app()->make('redis');
+    // // $redis->set("key1", "TEST");
+    // return $redis->get("key1");
+
+
+    return URL::temporarySignedRoute(
+        'unsubscribe',
+        now()->addSeconds(30),
+        ['user' => '1']
+    );
 });
+
+use Illuminate\Http\Request;
+
+Route::get('/unsubscribe/{user}', function (Request $request) {
+    if (!$request->hasValidSignature()) {
+        abort(401);
+    }
+    return "Thihazaw";
+    // ...
+})->name('unsubscribe');
